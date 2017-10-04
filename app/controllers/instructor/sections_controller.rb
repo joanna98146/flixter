81e834589ac20db9ec17f1ee4c6f135b1021d314
Joanna Tracy <joanna98146@yahoo.com>
2017-10-04 20:56:44 +0000

    sectiosn challenge to make them drag and drop, not sure if its right

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::SectionsController < ApplicationController
  before_action :authenticate_user!
  before_action :require_authorized_for_current_course
+ before_action :require_authorized_for_current_section, only: [:new, :create]
+ before_action :require_authorized_for_current_lesson, only: [:update]
  
  def new
     @section = Section.new
@@ -11,7 +13,10 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
   
-  
+  def update
+      current_lesson.update_attributes(section_params)
+      render plain: 'updated!'
+  end
   private
   
   def require_authorized_for_current_course
@@ -21,11 +26,15 @@ class Instructor::SectionsController < ApplicationController
   end
   
   helper_method :current_course
-  def current_course
+def current_course
+  if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
   end
+end
 
   def section_params
-    params.require(:section).permit(:title)
+   params.require(:section).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
