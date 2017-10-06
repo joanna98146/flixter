3b4110af14147cb9ec9b397e2bfec77a48d2ca7e
Joanna Tracy <joanna98146@yahoo.com>
2017-10-06 21:26:28 +0000

    lesson 35 added new section form model, removed instructorsectionnewhtmlerb per instructions

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,52 +3,42 @@ class Instructor::SectionsController < ApplicationController
  before_action :require_authorized_for_current_course, only: [:create]
  before_action :require_authorized_for_current_section, only: [:update]
 
- 
-  def new
-    @section = Section.new
-  end
 
   def create
-        @section = @current_course.sections.create(section_params)
-        redirect_to instructor_course_path(current_course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
-  
+
   def update
     current_section.update_attributes(section_params)
-    render plain: 'updated!'
+    render plain: 'ok!'
   end
-  
   
   private
  
   
-  helper_method :current_course
-   def current_course
-    if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
-    else
-      current_section.course
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
     end
-   end
-  
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render text: "Unauthorized", status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
-  
-   helper_method :current_section
-    def current_section
-        @current_section ||= Section.find(params[:id])
-    end
 
-  def require_authorized_for_current_section
-  if current_section.course.user != current_user
-   render text: 'Unauthorized', status: :unauthorized
-  end
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
   end
 
-   def section_params
-     params.require(:section).permit(:title, :row_order_position)
-   end
+  def section_params
+    params.require(:section).permit(:title, :row_order_position)
+  end
 end
